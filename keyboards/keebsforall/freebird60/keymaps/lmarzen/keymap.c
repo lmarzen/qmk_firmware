0d107f5f398daf06f84ca7f2211174d61e076fb9
Luke Marzen <marzenlj@gmail.com>
2022-11-26 19:19:01 -0600

    updated keymap

--- keyboards/keebsforall/freebird60/keymaps/lmarzen/keymap.c
+++ keyboards/keebsforall/freebird60/keymaps/lmarzen/keymap.c
@@ -20,20 +20,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-        MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENTER,
+        MO(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENTER,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), MO(1), MO(2), KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), MO(1), MO(3), KC_RCTL
     ),
-    [1] = LAYOUT(
+    [1] = LAYOUT( // function layer
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-        _______, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, _______, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_PAUS, KC_INS, KC_PSCR,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCRL, _______,
+        _______, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, _______, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_PAUS, KC_INS, KC_PSCR,
+        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_SCRL, _______,
         KC_LSFT, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_BRID, KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, _______, _______, KC_RCTL
     ),
-    [2] = LAYOUT(
+    [2] = LAYOUT( // function layer for caps lock key
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+        _______, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, _______, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_PAUS, KC_INS, KC_PSCR,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_SCRL, _______,
+        KC_LSFT, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_BRID, KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, _______, _______, KC_RCTL
+    ),
+    [3] = LAYOUT( // alternative function layer
         QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_TAB, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_LSFT, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_BRID, KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, _______, _______, KC_RCTL
