dc317388c810235ae5ef72319377d7575f95aa12
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-05-10 10:17:26 +0100

    feat: Incrementally merge wireless callbacks

--- keyboards/neo/neo65_trimode/neo65-trimode.c
+++ keyboards/neo/neo65_trimode/neo65-trimode.c
@@ -17,5 +17,104 @@
 #include QMK_KEYBOARD_H
 
 #ifdef WIRELESS_ENABLE
-#    include "wireless.h"
+    include "wireless.h"
+#endif
+
+void keyboard_post_init_kb(void) {
+
+//#ifdef CONSOLE_ENABLE
+//    debug_enable = true;
+//#endif
+
+//eeconfig_confinfo_init();
+
+//#ifdef LED_POWER_EN_PIN
+//    gpio_set_pin_output(LED_POWER_EN_PIN);
+//    gpio_write_pin_low(LED_POWER_EN_PIN);
+//#endif
+
+//#ifdef USB_POWER_EN_PIN
+//    gpio_write_pin_low(USB_POWER_EN_PIN);
+//    gpio_set_pin_output(USB_POWER_EN_PIN);
+//#endif
+
+#ifdef WIRELESS_ENABLE
+    wireless_init();
+    wireless_devs_change(!confinfo.devs, confinfo.devs, false);
+    post_init_timer = timer_read32();
+#endif
+
+    keyboard_post_init_user();
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+
+    if (process_record_user(keycode, record) != true) {
+        return false;
+    }
+
+#ifdef WIRELESS_ENABLE
+    if (process_record_wls(keycode, record) != true) {
+        return false;
+    }
+#endif
+
+    switch (keycode) {
+        default:
+            return true;
+    }
+
+    return false;
+}
+
+#ifdef WIRELESS_ENABLE
+bool process_record_wls(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t keycode_shadow                     = 0x00;
+    static deferred_token wls_process_long_press_token = INVALID_DEFERRED_TOKEN;
+
+    keycode_shadow = keycode;
+
+#    ifndef WLS_KEYCODE_PAIR_TIME
+#        define WLS_KEYCODE_PAIR_TIME 3000
+#    endif
+
+#    define WLS_KEYCODE_EXEC(wls_dev)                                                                                          \
+        do {                                                                                                                   \
+            if (record->event.pressed) {                                                                                       \
+                wireless_devs_change(wireless_get_current_devs(), wls_dev, false);                                             \
+                if (wls_process_long_press_token == INVALID_DEFERRED_TOKEN) {                                                  \
+                    wls_process_long_press_token = defer_exec(WLS_KEYCODE_PAIR_TIME, wls_process_long_press, &keycode_shadow); \
+                }                                                                                                              \
+            } else {                                                                                                           \
+                cancel_deferred_exec(wls_process_long_press_token);                                                            \
+                wls_process_long_press_token = INVALID_DEFERRED_TOKEN;                                                         \
+            }                                                                                                                  \
+        } while (false)
+
+    switch (keycode) {
+        case KC_BT1: {
+            WLS_KEYCODE_EXEC(DEVS_BT1);
+        } break;
+        case KC_BT2: {
+            WLS_KEYCODE_EXEC(DEVS_BT2);
+        } break;
+        case KC_BT3: {
+            WLS_KEYCODE_EXEC(DEVS_BT3);
+        } break;
+        case KC_2G4: {
+            WLS_KEYCODE_EXEC(DEVS_2G4);
+        } break;
+        case KC_USB: {
+            if (record->event.pressed) {
+                wireless_devs_change(wireless_get_current_devs(), DEVS_USB, false);
+            }
+        } break;
+        case KC_BATQ: {
+        } break;
+        default:
+            return true;
+    }
+
+    return false;
+}
 #endif
