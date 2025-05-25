2db6a8b0260f34db50c106b334d5ff954aaad11d
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-05-25 15:03:04 +0200

    feat: Start to implement indicators

--- keyboards/shortcut/bridge75_iso/bridge75_iso.c
+++ keyboards/shortcut/bridge75_iso/bridge75_iso.c
@@ -327,6 +327,22 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
         rgb_matrix_set_color(43, 0x77, 0x77, 0x77);
     }
 
+    // When not in default layer set all mapped keys to yellow
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED &&
+                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_YELLOW);
+                }
+            }
+        }
+    }
+
 #    ifdef WIRELESS_ENABLE
     rgb_matrix_wls_indicator();
 #    endif
