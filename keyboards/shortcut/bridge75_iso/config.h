8be8f9437d3ea57c5e44981f95b9417104559670
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-05-31 14:55:18 +0100

    fix: Go back to nearly zero config

--- keyboards/shortcut/bridge75_iso/config.h
+++ keyboards/shortcut/bridge75_iso/config.h
@@ -1,20 +1,7 @@
 // Copyright 2025 emolitor (github.com/emolitor)
-// Copyright 2024 Wind (@yelishang)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-
-/* PINS */
-#define USB_POWER_EN_PIN A14
-#define LED_POWER_EN_PIN B7
-#define BT_CABLE_PIN B8 // 充电接入时为高 High when charging
-#define BT_CHARGE_PIN B9 // 充电时为低，充满时为高 Low when charging, high when fully charged
-
-/* UART */
-#define UART_TX_PIN A9
-#define UART_TX_PAL_MODE 7
-#define UART_RX_PIN A10
-#define UART_RX_PAL_MODE 7
 
 /* FLASH */
 #define SPI_DRIVER SPIDQ
@@ -23,4 +10,3 @@
 #define SPI_MISO_PIN B4
 #define SPI_MOSI_PAL_MODE 5
 #define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
-#define WEAR_LEVELING_LOGICAL_SIZE (WEAR_LEVELING_BACKING_SIZE / 2)
--- keyboards/shortcut/bridge75_iso/halconf.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Wind (@yelishang)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define HAL_USE_SERIAL TRUE
-#define PAL_USE_CALLBACKS TRUE
-
-#include_next <halconf.h>
