f5cee85351a66f4af69ae80e5870fb525ad63dd4
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-06-20 13:01:12 +0100

    fix: Move to consolidated wireless branch of qmk_userspace_via

--- keyboards/shortcut/bridge75/config.h
+++ keyboards/shortcut/bridge75/config.h
@@ -11,6 +11,7 @@
  */
 #define WIRELESS_TAPPING_TERM 3000
 
+#ifdef RGB_MATRIX_ENABLE
 /* COLORS adjusted for RGB_MATRIX_MAXIMUM_BRIGHTNESS of 104 */
 #define RGB_ADJ_BLUE    0x00, 0x00, 0xC8
 #define RGB_ADJ_GREEN   0x00, 0xC8, 0x00
@@ -18,6 +19,7 @@
 #define RGB_ADJ_RED     0xC8, 0x00, 0x00
 #define RGB_ADJ_WHITE   0xC8, 0xC8, 0xC8
 #define RGB_ADJ_YELLOW  0xC8, 0x64, 0x00
+#endif
 
 /* KEYS */
 #define ESCAPE_INDEX    0
@@ -37,7 +39,9 @@
 
 /* POWER */
 #define USB_POWER_EN_PIN A14
+#ifdef RGB_MATRIX_ENABLE
 #define LED_POWER_EN_PIN B7
+#endif
 #define BT_CABLE_PIN B8 // High when charging
 #define BT_CHARGE_PIN B9 // Low when charging, high when fully charged
 
